489a35af98e27b19f107be6d13926d21577b7510
K-btr2004 <12yatavee.noei@gmail.com>
2024-11-11 05:18:31 +0000

    test case& correct code

--- test_zoo.py
+++ test_zoo.py
@@ -10,5 +10,49 @@ class TestZoo(unittest.TestCase):
        
     # Add your additional test cases here.
 
+    #Boundary Value Analysis
+    def test_Invalid_age_BVA(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid input")
+
+    def test_min_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+
+    def test_max_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+
+    def test_min_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+   
+    def test_max_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+
+    def test_min_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+
+    def test_max_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+    #Equivalent Claass Partitioning
+    def test_Invalid_age_ECP(self):
+        self.assertEqual(self.zoo.get_ticket_price(-2), "Invalid input")
+
+    def test_teen_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(30), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+
 if __name__ == '__main__':
     unittest.main()
